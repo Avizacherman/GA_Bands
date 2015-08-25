dae5c2907037e81aff0ebdee2e14c45faac01d0c
Avi Zacherman <avizacherman@gmail.com>
2015-08-25 19:37:00 -0400

    working on search

--- bruised_bacon/app/models/event.rb
+++ bruised_bacon/app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
 	belongs_to :band
 	belongs_to :venue
+
+	def self.search(search)
+		find(:all, :conditions => ['date LIKE ?', search])
+	end
 end
--- bruised_bacon/config/routes.rb
+++ bruised_bacon/config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
     resources :events
     resources :bands
 
+    get "/search" => "events#search"
+
   # Example resource route with options:
   #   resources :products do
   #     member do
