ed592e24db8aeca3fa4dc157626ceb448968b723
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2015-08-25 15:28:28 -0400

    tables

--- bruised_bacon/db/schema.rb
+++ bruised_bacon/db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825184313) do
+ActiveRecord::Schema.define(version: 20150825192316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bands", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "venue_id"
+    t.integer  "event_id"
+    t.string   "genre"
+    t.boolean  "explicit_lyrics"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.date     "date"
