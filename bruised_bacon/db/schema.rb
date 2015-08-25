5c86faf24268d990f0be7bcf4a7b05d39e323b6a
Avi Zacherman <avizacherman@gmail.com>
2015-08-25 18:04:09 -0400

    update views fix migration

--- bruised_bacon/db/schema.rb
+++ bruised_bacon/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825204732) do
+ActiveRecord::Schema.define(version: 20150825220247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150825204732) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.integer  "band_id"
+    t.string   "name"
   end
 
   create_table "venues", force: :cascade do |t|
